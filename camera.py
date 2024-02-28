3b9b66753f9b3d66740e4689fcbc3b5fe38e4e76
10053465 <ravi.dhiman@thalesgroup.com>
2024-02-28 15:42:59 +0530

    initial commit camera using streamlit

---
+++ camera.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from PIL import Image
+
+
+camera_image = st.camera_input("camera")
+
+if camera_image:
+    print(camera_image)
+    img = Image.open(camera_image)
+    gray_img = img.convert("L")
+    st.image(gray_img)
+
